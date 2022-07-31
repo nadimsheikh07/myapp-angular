9c08f50aa0e0dc37e6c8ed6540eb3963aae2ca92
Nadim.Sheikh <nadim.sheikh@dresma.com>
2022-07-31 13:41:14 +0530

    first commit

---
+++ src/app/post/index/index.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from "@angular/core";
+import { Post } from "../post";
+import { PostService } from "../post.service";
+
+@Component({
+  selector: "app-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.scss"]
+})
+export class IndexComponent implements OnInit {
+  posts: Post[] = [];
+  constructor(public postService: PostService) {}
+
+  ngOnInit(): void {
+    this.postService.getAll().subscribe((data: Post[]) => {
+      this.posts = data;
+      console.log(this.posts);
+    });
+  }
+
+  deletePost(id: number) {
+    this.postService.delete(id).subscribe(res => {
+      console.log(res);
+      this.posts = this.posts.filter(item => item.id !== id);
+      console.log("Post deleted successfully!");
+    });
+  }
+}
---
+++ src/app/post/post-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { IndexComponent } from './index/index.component';
+import { ViewComponent } from './view/view.component';
+
+const routes: Routes = [
+  { path: 'post', redirectTo: 'post/index', pathMatch: 'full'},
+
+  { path: 'post/index', component: IndexComponent },
+
+  { path: 'post/:postId/view', component: ViewComponent },
+
+  { path: 'post/create', component: CreateComponent },
+
+  { path: 'post/:postId/edit', component: EditComponent } 
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PostRoutingModule { }
